b004f4f36d87725ef1d5e44fa1bc18699b62fc94
Evan Turner <evturn@gmail.com>
2015-03-18 16:39:46 -0400

    fix made in database config

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-var config = require('./config');
+var express  	 = require('express');
+var app 		 	 = express();
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
+var mongoose   = require('mongoose');
+var config 	   = require('./config');
+var path 	   	 = require('path');
 
-mongoose.connect(config.database);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(config.database);
-console.log('Listening on port ' + config.port);
+
+app.use(morgan('dev'));
+
+mongoose.connect(config.database); 
+
+app.use(express.static(__dirname + '/public'));
+
+// var apiRoutes = require('./app/routes/api')(app, express);
+// app.use('/api', apiRoutes);
+
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
+
+app.listen(config.port);
+console.log('Express listening on port ' + config.port);
