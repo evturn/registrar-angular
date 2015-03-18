099a55308be44f9a4e7f51a7c7eb72bec51abfe3
Evan Turner <evturn@gmail.com>
2015-03-18 16:54:28 -0400

    route added for users requesting users/:user_id

--- app/routes/api.js
+++ app/routes/api.js
@@ -140,4 +140,49 @@ module.exports = function(app, express) {
 
 	  next(); // make sure we go to the next routes and don't stop here
 	});
+
+	// test route to make sure everything is working 
+	// accessed at GET http://localhost:8080/api
+	apiRouter.get('/', function(req, res) {
+		res.json({ message: 'hooray! welcome to our api!' });	
+	});
+
+	// on routes that end in /users
+	// ----------------------------------------------------
+	apiRouter.route('/users')
+
+		// create a user (accessed at POST http://localhost:8080/users)
+		.post(function(req, res) {
+			
+			var user = new User();		// create a new instance of the User model
+			user.name = req.body.name;  // set the users name (comes from the request)
+			user.username = req.body.username;  // set the users username (comes from the request)
+			user.password = req.body.password;  // set the users password (comes from the request)
+
+			user.save(function(err) {
+				if (err) {
+					// duplicate entry
+					if (err.code == 11000) 
+						return res.json({ success: false, message: 'A user with that username already exists. '});
+					else 
+						return res.send(err);
+				}
+
+				// return a message
+				res.json({ message: 'User created!' });
+			});
+
+		})
+
+		// get all the users (accessed at GET http://localhost:8080/api/users)
+		.get(function(req, res) {
+			User.find(function(err, users) {
+				if (err) res.send(err);
+
+				// return the users
+				res.json(users);
+			});
+		});
+
+
 };
