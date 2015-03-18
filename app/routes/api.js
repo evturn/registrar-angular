0f1cc36619df84c4f106619f4f249401c91c5953
Evan Turner <evturn@gmail.com>
2015-03-18 16:53:16 -0400

    route handler for request params to verify secret or return 403 response

--- app/routes/api.js
+++ app/routes/api.js
@@ -58,6 +58,86 @@ module.exports = function(app, express) {
 	  });
 	});
 
+	apiRouter.post('/authenticate', function(req, res) {
+		console.log(req.body.username);
 
+	  // find the user
+	  User.findOne({
+	    username: req.body.username
+	  }).select('name username password').exec(function(err, user) {
 
+	    if (err) throw err;
+
+	    // no user with that username was found
+	    if (!user) {
+	      res.json({ 
+	      	success: false, 
+	      	message: 'Authentication failed. User not found.' 
+	  		});
+	    } else if (user) {
+
+	      // check if password matches
+	      var validPassword = user.comparePassword(req.body.password);
+	      if (!validPassword) {
+	        res.json({ 
+	        	success: false, 
+	        	message: 'Authentication failed. Wrong password.' 
+	    		});
+	      } else {
+
+	        // if user is found and password is right
+	        // create a token
+	        var token = jwt.sign({
+	        	name: user.name,
+	        	username: user.username
+	        }, superSecret, {
+	          expiresInMinutes: 1440 // expires in 24 hours
+	        });
+
+	        // return the information including token as JSON
+	        res.json({
+	          success: true,
+	          message: 'Enjoy your token!',
+	          token: token
+	        });
+	      }   
+
+	    }
+
+	  });
+	});
+
+	// route middleware to verify a token
+	apiRouter.use(function(req, res, next) {
+		// do logging
+		console.log('Somebody just came to our app!');
+
+	  // check header or url parameters or post parameters for token
+	  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+	  // decode token
+	  if (token) {
+
+	    // verifies secret and checks exp
+	    jwt.verify(token, superSecret, function(err, decoded) {      
+	      if (err)
+	        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+	      else
+	        // if everything is good, save to request for use in other routes
+	        req.decoded = decoded;    
+	    });
+
+	  } else {
+
+	    // if there is no token
+	    // return an HTTP response of 403 (access forbidden) and an error message
+   	 	return res.status(403).send({ 
+   	 		success: false, 
+   	 		message: 'No token provided.' 
+   	 	});
+	    
+	  }
+
+	  next(); // make sure we go to the next routes and don't stop here
+	});
 };
