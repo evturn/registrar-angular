9941c6e40133bb85f317ac656eb1c2d6d567bf94
Evan Turner <evturn@gmail.com>
2015-03-18 16:51:31 -0400

    route handler added to find user and verify username and password, then create token if correct

--- app/routes/api.js
+++ app/routes/api.js
@@ -11,6 +11,53 @@ module.exports = function(app, express) {
 	//authenticate a user
 	apiRouter.post('/authenticate', function(req, res) {
 		console.log(req.body.username);
-	})
+
+	  // find the user
+	  User.findOne({
+	    username: req.body.username
+	  }).select('name username password').exec(function(err, user) {
+
+	    if (err) throw err;
+
+	    // no user with that username was found
+	    if (!user) {
+	      res.json({ 
+	      	success: false, 
+	      	message: 'Authentication failed. User not found.' 
+	  		});
+	    } else if (user) {
+
+	      // check if password matches
+	      var validPassword = user.comparePassword(req.body.password);
+	      if (!validPassword) {
+	        res.json({ 
+	        	success: false, 
+	        	message: 'Authentication failed. Wrong password.' 
+	    		});
+	      } else {
+
+	        // if user is found and password is right
+	        // create a token
+	        var token = jwt.sign({
+	        	name: user.name,
+	        	username: user.username
+	        }, superSecret, {
+	          expiresInMinutes: 1440 // expires in 24 hours
+	        });
+
+	        // return the information including token as JSON
+	        res.json({
+	          success: true,
+	          message: 'Enjoy your token!',
+	          token: token
+	        });
+	      }   
+
+	    }
+
+	  });
+	});
+
+
 
 };
