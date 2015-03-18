3f81d3c82537d412a41fb84df401f7715cb6f7ae
Evan Turner <evturn@gmail.com>
2015-03-18 16:58:53 -0400

    apiRoutes module exported into server

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ mongoose.connect(config.database);
 
 app.use(express.static(__dirname + '/public'));
 
+var apiRoutes	 = require('./app/routes/api')(app, express);
+app.use('/api', apiRoutes);
+
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
