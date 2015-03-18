c2ff4efce91089379a4705bf686cf7c134782260
Evan Turner <evturn@gmail.com>
2015-03-18 17:51:34 -0400

    .get for a single user created in user service

--- public/app/services/userService.js
+++ public/app/services/userService.js
@@ -2,4 +2,10 @@ angular.module('userService'), [])
 
 .factory('User', function($http) {
 
+	var userFactory = {};
+
+	userFactory.get = function(id) {
+		return $http.get('/api/users/' + id);
+	};
+
 });
