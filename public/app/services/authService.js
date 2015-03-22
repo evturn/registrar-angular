e68ca946f2d7d65d67d1dc6d9f2ade94a9ca3368
Evan Turner <evturn@gmail.com>
2015-03-22 16:30:27 -0400

    setToken function for authTokenFactory created

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -12,6 +12,16 @@ angular.module('authService', [])
 
 	var authTokenFactory = {};
 
+	authTokenFactory.getToken = function() {
+		return $window.localStorage.getItem('token');
+	};
+	
+	authTokenFactory.setToken = function(token) {
+		if (token)
+			$window.localStorage.setItem('token', token);
+	 	else
+			$window.localStorage.removeItem('token');
+	};
 
 	return authTokenFactory;
 
