c2cb6e0526b69440c5c36d0e628bca9a2100ce52
Evan Turner <evturn@gmail.com>
2015-03-23 15:26:34 -0400

    app configuration to integrate token into requests created

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -62,6 +62,26 @@ angular.module('authService', [])
 
 	var interceptorFactory = {};
 
+	interceptorFactory.request = function(config) {
+
+		var token = AuthToken.getToken();
+
+		if (token) 
+			config.headers['x-access-token'] = token;
+		
+		return config;
+	};
+
+	interceptorFactory.responseError = function(response) {
+
+		if (response.status == 403) {
+			AuthToken.setToken();
+			$location.path('/login');
+		}
+
+		return $q.reject(response);
+	};
+
 	return interceptorFactory;
 
 });
